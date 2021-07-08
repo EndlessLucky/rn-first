2589c5183b19bbde4a8bd03e74eb175defe4fe6e
Olive105 <105.olivesoft@gmail.com>
2021-07-08 09:40:40 +0900

    feat - implement FlatList

--- App.js
+++ App.js
@@ -1,41 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, TextInput, Button } from 'react-native';
-
-const Cat = (props) => {
-  const [ isHungry, setIsHungry ] = useState(true);
-
-  return(
-    <View>
-      <Text>
-        I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}!
-      </Text>
-      <Button
-        onPress = {() => {
-          setIsHungry(false);
-        }}
-        disabled = {!isHungry}
-        title = {isHungry? 'Pour me some milk, please!' : 'Thank you'}
-      />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Image, ScrollView, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <Cat name="Munkustrap" />
-      <Cat name="Spot" />
-    </>
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem = {({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'cyan',
-    color: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingTop: 22
+   },
+   item: {
+     padding: 10,
+     fontSize: 18,
+     height: 44,
+   },
 });
